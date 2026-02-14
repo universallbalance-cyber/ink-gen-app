9e0e4894bbad765de90d023c3d00db03ece81042
universallbalance-cyber <universallbalance@gmail.com>
2026-02-13 19:40:21 -0700

    Update env.js

--- env.js
+++ env.js
@@ -1,3 +1,2 @@
-// This file will be hidden from GitHub
-window.GOOGLE_API_KEY = "AIzaSyAF0X8coR8_sLYOaRwVH3L7FUlcaEmLRcw";
+window.SUPABASE_URL = "https://your-project-id.supabase.co/functions/v1/gemini-proxy";
 
